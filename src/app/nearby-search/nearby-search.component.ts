09fc76a94b047fc2027176792c5fa1466500e028
Ishfaq <rohaan.ishfaq@teradata.com>
2017-08-20 12:15:28 +0500

    changes

--- src/app/nearby-search/nearby-search.component.ts
+++ src/app/nearby-search/nearby-search.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { StepState } from '@covalent/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { StepState, TdSearchInputComponent } from '@covalent/core';
 
 @Component({
     selector: 'nearby-search',
@@ -20,6 +20,24 @@ export class NearBySearchComponent implements OnInit {
         'Tomato'
     ];
 
+    locations: string[] = [
+        'Rawal Town',                            
+        'Maira Sumbal Jaffar',    
+        'Bokra',    
+        'Jhangi Saydan',    
+        'Badhana Kalan',    
+        'Tarnol',    
+        'Sarai Kharbooza',    
+        'Shah Allah Ditta',    
+        'Golra Sharif',    
+        'Ali Pur Farash',
+        'Taramri',
+        'Chak Shahzad',    
+        'Pandori'
+    ];
+
+    filteredLocations: string[];
+    
     showResult: boolean = false;
 
     filteredStrings: string[];
@@ -30,14 +48,19 @@ export class NearBySearchComponent implements OnInit {
     mode = 'determinate';
     bufferValue = 75;
 
-    searchInputTerm: string;
+    @ViewChild('search') search: TdSearchInputComponent;
 
     @Input() searchbarType: string;
 
+    selectedLocation: string;
+
     ngOnInit(): void {
         this.filterStrings('');
+        this.filteredLocations = this.locations;
     }
 
+    showNoLocationMsg: boolean;
+    
     filterStrings(value: string): void {
         this.filteredStrings = this.strings.filter((item: any) => {
             if (value) {
@@ -50,5 +73,38 @@ export class NearBySearchComponent implements OnInit {
         });
     }
 
+    filterLocations(value): void {
+        
+        this.filteredLocations = this.locations.filter(l =>  l.toLowerCase().indexOf(value.toLowerCase()) != -1 );
+        
+        if(value.length != 0 && this.filteredLocations.length == 0){
+            this.showNoLocationMsg = true;
+            setTimeout(() => {
+                this.showNoLocationMsg = false;
+            }, 1500);
+        }
+    }
 
+    clear(): void {
+        this.search.value = '';
+        this.showResult = false;
+    }
+
+    selectLocation(loc): void {
+        this.search.value = loc;
+        this.selectedLocation = loc;
+        this.filteredLocations = [];
+    }
+
+    doSearch(value): void {
+        this.selectedLocation = value;
+        if(this.search.value)
+            this.showResult = true;
+    }
+
+    searchLoc(): void {
+        if(this.search.value)
+            this.showResult = true;
+    }
+ 
 }
